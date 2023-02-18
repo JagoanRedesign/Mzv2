e1d81b239c14d4c15998efc160b78a806f84ef1e
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-02-18 07:49:22 +0700

    Add files via upload

---
+++ Kazu/modules/bot/start.py
@@ -0,0 +1,11 @@
+#Credit Bye Geez|Ram
+#Thanks To All Dev
+
+
+from Kazu import app
+from pyrogram import filters
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+   await message.reply_text("PyroZu-Userbot Telah Aktif")
