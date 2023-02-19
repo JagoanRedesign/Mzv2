5837159428e51b3f959736ea053a8f95a335a95a
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-02-19 11:59:21 +0700

    Update asupan.py

--- Kazu/modules/asupan.py
+++ Kazu/modules/asupan.py
@@ -22,7 +22,67 @@ async def asupan_cmd(client: Client, message: Message):
                 [
                     asupan.video.file_id
                     async for asupan in client.search_messages(
-                        "tedeasupancache", filter=enums.MessagesFilter.VIDEO
+                        "punyakenkan", filter=enums.MessagesFilter.VIDEO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
+
+@Client.on_message(filters.command(["ayang", "ayng"], cmd) & filters.me)
+async def ayang_cmd(client: Client, message: Message):
+    Kazu = await edit_or_reply(message, "🔎 `Search Ayang...`")
+    await gather(
+        Kazu.delete(),
+        client.send_photo(
+            message.chat.id,
+            choice(
+                [
+                    ayang.photo.file_id
+                    async for ayang in client.search_messages(
+                        "CeweLogoPack", filter=enums.MessagesFilter.PHOTO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
+
+@Client.on_message(filters.command(["mzpp", "ppcp"], cmd) & filters.me)
+async def couple_cmd(client: Client, message: Message):
+    Kazu = await edit_or_reply(message, "🔎 `Search PP Couple...`")
+    await gather(
+        Kazu.delete(),
+        client.send_photo(
+            message.chat.id,
+            choice(
+                [
+                    couple.photo.file_id
+                    async for couple in client.search_messages(
+                        "ppcpcilik", filter=enums.MessagesFilter.PHOTO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
+
+@Client.on_message(filters.command(["bokep", "bkp"], cmd) & filters.me)
+async def bokep_cmd(client: Client, message: Message):
+    Kazu = await edit_or_reply(message, "`Mencari bahan...`")
+    await gather(
+        Kazu.delete(),
+        client.send_video(
+            message.chat.id,
+            choice(
+                [
+                    bokep.video.file_id
+                    async for bokep in client.search_messages(
+                        "bahaninimah", filter=enums.MessagesFilter.VIDEO
                     )
                 ]
             ),
@@ -37,6 +97,10 @@ add_command_help(
         [
             f"asupan atau {cmd}ptl",
             "Untuk Mengirim video asupan secara random.",
-        ]
+        ],
+         [f"{cmd}ayang", "Mencari Foto ayang kamu /nNote: Modul ini buat cwo yang jomblo."],
+        [f"{cmd}ppcp", "Mencari Foto PP Couple Random."],
+        [f"{cmd}bokep", "to send random porno videos."]
+       
     ],
 )
