d1c1b91b805fd2dfc603e6037711e7ff0415b5b2
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-21 14:50:07 +0700

    Update start.py

--- Kazu/modules/bot/start.py
+++ Kazu/modules/bot/start.py
@@ -9,3 +9,9 @@ from pyrogram import filters
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
    await message.reply_text("Mz Userbot Telah Aktif")
+
+
+
+@app.on_message(filters.command("help") & filters.private)
+async def start(client, message):
+   await message.reply_text("ini help")
